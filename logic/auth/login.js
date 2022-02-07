8e00304f16f495b2a88a88218bb859c3eb00bfc4
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-07 00:21:32 -0500

    update login.js

--- logic/auth/login.js
+++ logic/auth/login.js
@@ -3,6 +3,12 @@ const fs = require("fs");
 const authenticator = require("./authenticator.js");
 const bcrypt = require("bcryptjs");
 
+/* function login
+* @Method : POST
+* @param req {body : {username , password , token}}
+* @param res {}
+*/
+
 const login = (req, res) => {
     if (!req.body) return;
     let username,
@@ -19,43 +25,47 @@ const login = (req, res) => {
             "Ups!\nA ocurrido un error. Si este problema continua , reportelo.",
             error: err
         });
+        return;
     }
 
-    if (token != config.APPTOKEN)
-        return {
-        status: false,
-        message: "Esta usando una aplicación obsoleta o de terceros. Verifique."
-    };
+    if (token != config.APPTOKEN) {
+        res.json({
+            status: false,
+            message: "Esta usando una aplicación obsoleta o de terceros. Verifique."
+        });
+        return;
+    }
 
-    //comprobamos si existe el usuario
-    if (!fs.existsSync(config.DB + "/accounts/" + username + ".json"))
-        return {
-        status: false,
-        message: "Nombre de usuario o contraseña incorrectos."
-    };
-    //si existe almacenamos los datos momentaneamente
-    let account = helper.readFile(config.DB + "/accounts/" + username + ".json");
-    if (!account) return {
-        status: false,
-        message: "Ups! \nOcurrio un error"
-    };
-    //comprobamos si las contraseñas coinciden
-    if (!bcrypt.compareSync(password, account.password))
-        return {
-        status: false,
-        message: "Nombre de usuario o contraseña incorrectos."
-    };
-    //si las contraseñas coinciden damos acceso al usuario
-    //comprobamos si la cuenta tiene un id
-    if (account.id == null || account.id == undefined)
-        return {
-        status: false,
-        message: "La cuenta se encuentra corrupta. Reporte este error."
-    };
-    return {
+
+    if (global.users[username] != undefined) {
+        res.json({
+            status: false,
+            message: "Nombre de usuario o contraseña incorrectos."
+        });
+        return;
+    }
+
+    const account = global.users[username];
+
+    if (!bcrypt.compareSync(password, account.password)) {
+        res.json({
+            status: false,
+            message: "Nombre de usuario o contraseña incorrectos."
+        });
+        return
+    }
+
+    if (account.id == null || account.id == undefined) {
+        res.json({
+            status: false,
+            message: "La cuenta se encuentra corrupta. Reporte este error."
+        });
+        return;
+    }
+    res.json({
         status: true,
         message: authenticator.generateToken(account.id)
-    };
+    });
 }
 
 module.exports = login;
