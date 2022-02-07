9578f0408decc6e2b766d341449e312bf12ca78c
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-07 14:15:27 -0500

    update verifyMail.js

--- logic/auth/verifyMail.js
+++ logic/auth/verifyMail.js
@@ -1,22 +1,34 @@
 const config = require("../../config.js");
 const verifyMail = (req, res) => {
-  if (!req.params && !req.params.user && !req.param.id) {
-      res.json({status : false , message : "error"});
-    return;
-  }
-  let username = req.params.user;
-  let id = req.params.id;
+    if (!req.params && !req.params.user && !req.param.id) {
+        res.json({
+            status: false, message: "error"
+        });
+        return;
+    }
+    let username = req.params.user;
+    let id = req.params.id;
 
-  if (global.users[username] == undefined) {
-    res.json({ status : false ,message: "No exist" });
-    return;
-  }
+    if (global.users[username] == undefined) {
+        res.json({
+            status: false, message: "No exist"
+        });
+        return;
+    }
 
-  if (global.users[username].verified == true) return { message: "already verified" };
-  if (global.users[username].id != id){ res.json({ message: "wrong url" })};
-  global.users[username].verified = true;
-  global.users[username].subscribed = true;
-  res.json({status : true , message: "verified" });
+    if (global.users[username].verified == true) return {
+        message: "already verified"
+    };
+    if (global.users[username].id != id) {
+        res.json({
+            status: false,
+            message: "wrong url"
+        })};
+    global.users[username].verified = true;
+    global.users[username].subscribed = true;
+    res.json({
+        status: true, message: "verified"
+    });
 }
 
 module.exports = verifyMail;
