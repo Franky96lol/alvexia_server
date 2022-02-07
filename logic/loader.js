8114a1157110e8d1e6267e322404d1ff3705b2ee
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-06 23:20:58 -0500

    update loader.js

--- logic/loader.js
+++ logic/loader.js
@@ -5,8 +5,12 @@ const config = require("../config.js");
 
 /* Loading users from database to server */
 const users = () => {
-    const accounts = fs.readdirSync(config.DB + "/accounts");
+    const accounts = fs.readdirSync(config.DB + "/accounts/");
     for (let acc of accounts) {
         global.users[acc.replace(".json" , "")] = fs.readFileSync(config.DB + "/accounts/" + acc , "utf-8");
     }
+};
+
+module.exports = {
+    users
 };
