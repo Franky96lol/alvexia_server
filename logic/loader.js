b9e9bd03952638a285e1a744ce6ae5f3bd4da5eb
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-06 23:16:42 -0500

    update loader.js

--- logic/loader.js
+++ logic/loader.js
@@ -3,6 +3,10 @@
 const fs = require("fs");
 const config = require("../config.js");
 
+/* Loading users from database to server */
 const users = () => {
-    let accounts = 
+    const accounts = fs.readdirSync(config.DB + "/accounts");
+    for(let acc of accounts){
+        
+    }
 };
