9818113f4e6506fc07602f9099e47d2073c13d28
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-07 13:37:45 -0500

    update verifyMail.js

--- logic/auth/verifyMail.js
+++ logic/auth/verifyMail.js
@@ -1,1 +1,26 @@
-{"current_user_url":"https://api.github.com/user","current_user_authorizations_html_url":"https://github.com/settings/connections/applications{/client_id}","authorizations_url":"https://api.github.com/authorizations","code_search_url":"https://api.github.com/search/code?q={query}{&page,per_page,sort,order}","commit_search_url":"https://api.github.com/search/commits?q={query}{&page,per_page,sort,order}","emails_url":"https://api.github.com/user/emails","emojis_url":"https://api.github.com/emojis","events_url":"https://api.github.com/events","feeds_url":"https://api.github.com/feeds","followers_url":"https://api.github.com/user/followers","following_url":"https://api.github.com/user/following{/target}","gists_url":"https://api.github.com/gists{/gist_id}","hub_url":"https://api.github.com/hub","issue_search_url":"https://api.github.com/search/issues?q={query}{&page,per_page,sort,order}","issues_url":"https://api.github.com/issues","keys_url":"https://api.github.com/user/keys","label_search_url":"https://api.github.com/search/labels?q={query}&repository_id={repository_id}{&page,per_page}","notifications_url":"https://api.github.com/notifications","organization_url":"https://api.github.com/orgs/{org}","organization_repositories_url":"https://api.github.com/orgs/{org}/repos{?type,page,per_page,sort}","organization_teams_url":"https://api.github.com/orgs/{org}/teams","public_gists_url":"https://api.github.com/gists/public","rate_limit_url":"https://api.github.com/rate_limit","repository_url":"https://api.github.com/repos/{owner}/{repo}","repository_search_url":"https://api.github.com/search/repositories?q={query}{&page,per_page,sort,order}","current_user_repositories_url":"https://api.github.com/user/repos{?type,page,per_page,sort}","starred_url":"https://api.github.com/user/starred{/owner}{/repo}","starred_gists_url":"https://api.github.com/gists/starred","topic_search_url":"https://api.github.com/search/topics?q={query}{&page,per_page}","user_url":"https://api.github.com/users/{user}","user_organizations_url":"https://api.github.com/user/orgs","user_repositories_url":"https://api.github.com/users/{user}/repos{?type,page,per_page,sort}","user_search_url":"https://api.github.com/search/users?q={query}{&page,per_page,sort,order}"}
+const config = require("../../config.js");
+const helper = require(config.LOGIC + "/helper.js");
+const fs = require("fs");
+
+function verifyMail(req, res) {
+  if (!req.params && !req.params.user && !req.param.id) {
+    return;
+  }
+  let username = req.params.user;
+  let id = req.params.id;
+
+  if (!fs.existsSync(config.DB + "/accounts/" + username + ".json")) {
+    return { message: "No exist" };
+  }
+
+  let acc = helper.readFile(config.DB + "/accounts/" + username + ".json");
+  if (acc.verified == true) return { message: "already verified" };
+  if (acc.id != id) return { message: "wrong url" };
+  acc.verified = true;
+  acc.subscribed = true;
+
+  helper.writeFile(config.DB + "/accounts/" + username + ".json", acc);
+  return { message: "verified" };
+}
+
+module.exports = verifyMail;
