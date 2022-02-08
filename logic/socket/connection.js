ca6d01f0785c5317396bd674ef4fef820fe29bef
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-08 17:10:42 -0500

    update connection.js

--- logic/socket/connection.js
+++ logic/socket/connection.js
@@ -1,14 +1,14 @@
 /* On Connection */
+const config = require("../../config.js");
+const statscalc = require(config.LOGIC + "/engine/statscalc.js");
 
 const connection = (io, socket, username) => {
     const acc = JSON.parse(JSON.stringify(global.users[username]));
-    const pjstats = {
-        
-    };
+    const pjstats = statscalc(username);
     socket.join("map_" + acc.pos.map);
-    socket.emit("player", acc);
-    socket.broadcast.to("map_" + acc.pos.map).emit("new_pj" , acc);
-    global.world[acc.pos.map].pjs[username] = acc;
+    socket.emit("pjstats", pjstats);
+    socket.broadcast.to("map_" + acc.pos.map).emit("new_pj" , pjstats);
+    global.world[acc.pos.map].pjs[username] = pjstats;
     socket.emit("load_map", global.world[acc.pos.map]);
 };
 
