2658c93d44345deee752756f4947485b6c570bab
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-08 09:46:08 -0500

    update connection.js

--- logic/socket/connection.js
+++ logic/socket/connection.js
@@ -1,1 +1,11 @@
 /* On Connection */
+
+const connection = (io, socket, username) => {
+    const acc = JSON.parse(JSON.stringify(global.users[username]));
+    delete acc.password;
+    socket.join("map_" + acc.pos.map);
+    socket.broadcast.to("map_" + acc.pos.map)
+    global.world[acc.pos.map].pjs[username] = acc;
+    socket.emit("player", acc);
+    socket.emit("map", global.world[acc.pos.map]);
+}
