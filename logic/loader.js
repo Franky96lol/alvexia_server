bffd5c0ce99d5a71ca68fec28e080ed38150ff27
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-07 21:37:41 -0500

    update loader.js

--- logic/loader.js
+++ logic/loader.js
@@ -7,15 +7,15 @@ const config = require("../config.js");
 const users = () => {
     const accounts = fs.readdirSync(config.DB + "/accounts/");
     for (let acc of accounts) {
-        global.users[acc.replace(".json" , "")] = JSON.stringify(fs.readFileSync(config.DB + "/accounts/" + acc , "utf-8"));
+        global.users[acc.replace(".json", "")] = JSON.stringify(fs.readFileSync(config.DB + "/accounts/" + acc, "utf-8"));
     }
 };
 
 /* Loading world from database to server */
 const world = () => {
     const worlds = fs.readdirSync(config.DB + "/maps/");
-    for(let map of worlds){
-        global.map[map.replace(".json" , "")] = JSON.stringify(fs.readFileSync(config.DB + "/maps/" + map , "utf-8"));
+    for (let map of worlds) {
+        global.world[map.replace(".json", "")] = JSON.stringify(fs.readFileSync(config.DB + "/maps/" + map, "utf-8"));
     }
 };
 
