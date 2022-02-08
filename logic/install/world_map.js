f2370f146d5ba90ec0bfc3cd4116baae11f20f27
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-07 19:15:55 -0500

    update world_map.js

--- logic/install/world_map.js
+++ logic/install/world_map.js
@@ -3,7 +3,11 @@
 const fs = require("fs");
 
 const generate = (map_x , map_y , mmap_x , mmap_y) => {
-    
+    for(let y = 0 ; y < map_y ; y++){
+        for(let x = 0 ; x < map_x ; x++){
+            
+        }
+    }
 }
 
 module.exports = generate;
