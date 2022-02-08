7f5b7ba38e008769755fd9d8fb7377b2a4fdd1e8
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-08 14:13:23 -0500

    update statscalc.js

--- logic/engine/statscalc.js
+++ logic/engine/statscalc.js
@@ -1,1 +1,12 @@
 /* Stadistics Calculator */
+const config = require("../../config.js");
+
+const statscalc = (username){
+    const acc = JSON.parse(JSON.stringify(global.users[username]));
+    delete acc.attributes.points;
+    const attr = acc.attributes;
+    const status = acc.status;
+    const equipment = acc.equipment;
+};
+
+module.exports = statscalc;
