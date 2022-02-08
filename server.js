c4dd86afba7b1b01f034ddfd18acdc6d975c8af6
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-08 09:48:49 -0500

    update server.js

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ const socketing = require(config.LOGIC + "/socketing.js");
 const bodyParser = require("body-parser");
 const auth = require(config.LOGIC + "/auth/authenticator.js");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 
 /*Generating World*/
 //require(config.LOGIC + "/install/world_map.js")(50 , 80);
@@ -31,20 +33,43 @@ global.world = [];
 loader.world();
 
 /* Express router */
-app.use("/", router); 
+app.use("/", router);
 // Wakeup route
-app.post("/wakeup" , (req , res) =>{
-    res.json({status : true});
+app.post("/wakeup", (req, res) => {
+    res.json({
+        status: true
+    });
 });
 //Error route
-app.use((req, res) => res.json({status : false , message: "ERROR 404" })); 
+app.use((req, res) => res.json({
+    status: false, message: "ERROR 404"
+}));
 
-/* Http Server Start */ 
+/* Http Server Start */
 server.listen(config.PORT, () => console.log("HTTP server running on port " + config.PORT));
 
 /* Socket Connection */
-io.on("connection" , (socket) => {
-    socketing(io , socket , username);
+io.on("connection", (socket) => {
+    if (socket.handshake.query.username == undefined || socket.handshake.query.token == undefined) {
+        socket.disconnect();
+        return;
+    }
+    const username = socket.handshake.query.username,
+    token = socket.handshake.query.token;
+    const verify = auth.verify(token);
+    if (verify == false) {
+        socket.disconnect();
+        return;
+    }
+    if (global.users[username] == undefined) {
+        socket.disconnect();
+        return;
+    }
+    if (global.users[username].id != verify) {
+        socket.disconnect();
+        return;
+    }
+    socketing(io, socket, username);
 });
 
 console.timeEnd("Server loaded in")
