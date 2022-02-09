5ea367e914bef9aedfe44bcdd690b3fc5fc8230b
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-09 14:11:19 -0500

    update disconnect.js

--- logic/socket/disconnect.js
+++ logic/socket/disconnect.js
@@ -1,1 +1,13 @@
 /* On disconnect */
+
+const config = require("../../config.js");
+
+const disconnect = (io , socket , username) => {
+    socket.broadcast.to("map_" + global.users[username].pos.map).emit("del_pj" , username);
+    delete global.world[global.users[username].pos.map].pjs[username];
+    socket.leave("map_" + global.users[username].pos.map);
+    global.users[username].isOnline = false;
+    return;
+};
+
+module.exports = disconnect;
