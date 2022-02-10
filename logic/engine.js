64f7b635cdfe001ad6300b583abb8a8726f3d839
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-10 18:29:08 -0500

    update engine.js

--- logic/engine.js
+++ logic/engine.js
@@ -1,1 +1,16 @@
 /* Game Engine */
+
+const config = require("../config.js");
+const saver = require(config.LOGIC + "/saver.js");
+
+class Engine {
+    constructor(){
+        
+    }
+    
+    start(){
+        saver.start();
+    }
+}
+
+module.exports = Engine;
