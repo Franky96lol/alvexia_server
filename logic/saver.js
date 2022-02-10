2a5bedfed39af9aaebbc3307d91e6c100f23c2f0
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-10 17:52:59 -0500

    update saver.js

--- logic/saver.js
+++ logic/saver.js
@@ -1,3 +1,34 @@
 /* Accounts and Assets saving function */
 
 const config = require("../config.js");
+const {TaskTimer} = require("tasktimer");
+const fs = require("fs");
+
+const timer = new TaskTimer(1000);
+timer.add([{
+    id: 'users_save',
+    tickInterval: 60,
+    totalRuns: 0,
+    callback(task) {
+        
+    }
+}, {
+    id: "world_save",
+    tickInterval : 60,
+    totalRuns : 0,
+    callback(task) {
+        
+    }
+}]);
+
+const users_save = () => {
+    
+};
+
+const start = () => {
+    timer.start();
+};
+
+const stop = () => {
+    timer.stop();
+};
