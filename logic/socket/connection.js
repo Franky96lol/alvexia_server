6202a483a69b709daaa8d1056b67ba3cfc856b77
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-10 17:02:28 -0500

    update connection.js

--- logic/socket/connection.js
+++ logic/socket/connection.js
@@ -6,18 +6,22 @@ const connection = (io, socket, username) => {
     const acc = JSON.parse(JSON.stringify(global.users[username]));
     const _stats = statscalc(username);
     const pjstats = {
-        nickname : acc.nickname,
-        level : acc.level,
-        xp : _stats.exp.xp,
-        c_xp : acc.xp,
-        hp : _stats.attr.hp,
-        mp : _stats.attr.mp,
-        c_hp : _stats.status.hp,
-        c_mp : _stats.status.mp,
-        pos : acc.pos
+        nickname: acc.nickname,
+        status: {
+            level: acc.level,
+            xp: _stats.exp.xp,
+            c_xp: acc.xp,
+            hp: _stats.attr.hp,
+            mp: _stats.attr.mp,
+            c_hp: _stats.status.hp,
+            c_mp: _stats.status.mp,
+        },
+        pos: acc.pos
     };
     socket.join("map_" + acc.pos.map);
-    socket.broadcast.to("map_" + acc.pos.map).emit("new_pj" , {username : username , pjstats : pjstats});
+    socket.broadcast.to("map_" + acc.pos.map).emit("new_pj", {
+        username: username, pjstats: pjstats
+    });
     global.world[acc.pos.map].pjs[username] = pjstats;
     socket.emit("load_map", global.world[acc.pos.map]);
     global.users[username].isOnline = true;
