920b6ea7327a0245257a34686a2f60b74f7e6f1e
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-11 16:13:33 -0500

    update world_material_generator.js

--- logic/engine/world_material_generator.js
+++ logic/engine/world_material_generator.js
@@ -1,1 +1,27 @@
 /* Material generator */
+
+const config = require("../../config");
+const {TaskTimer} = require("tasktimer")
+
+const timer = new TaskTimer(1000);
+timer.add([{
+    id: 'material_gen',
+    tickInterval: config.RATE.material_gen,
+    totalRuns: 0,
+    callback(task) {
+        material_generator();
+    }
+}]);
+
+const material_generator = (io) => {
+    for(let map in global.world){
+        for(let obj in global.world[map].objects){
+            if(global.world[map].objects[obj].ammount < global.world[map].objects[obj].max_ammount){
+                global.world[map].objects[obj].ammount += 1;
+                io.to("map_" + map).emit("obj_changed" , {id : obj , object : global.world[map].objects[obj]});
+            }
+        }
+    }
+};
+
+module.exports = material_generator;
