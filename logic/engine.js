5eeadb61baa68f4041da70ba8c89b2d6a3b268d1
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-11 16:19:04 -0500

    update engine.js

--- logic/engine.js
+++ logic/engine.js
@@ -2,14 +2,16 @@
 
 const config = require("../config.js");
 const saver = require(config.LOGIC + "/saver.js");
+const wmg = require(config.LOGIC + "/engine/world_material_generator.js");
 
 class Engine {
-    constructor(){
-        
+    constructor(io){
+        this.io = io;
     }
     
     start(){
         saver.start();
+        wmg.start(io);
     }
 }
 
