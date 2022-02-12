e9aa25508d10599dceefd2b527d8502843f374c4
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-11 19:32:08 -0500

    update chat.js

--- logic/socket/chat.js
+++ logic/socket/chat.js
@@ -1,1 +1,11 @@
 /* Chat Engine */
+
+const config = require("../../config.js");
+
+const chat = (io , socket , username) => {
+    io.on("message" , (data) => {
+        
+    });
+};
+
+module.exports = chat;
