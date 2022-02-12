d2037d1ce3cbaf979219f74b9faa8fff2e100d33
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-12 09:39:06 -0500

    update movement.js

--- logic/socket/movement.js
+++ logic/socket/movement.js
@@ -13,8 +13,8 @@ const movement = (io , socket , username) => {
         const x = (data.x / 100 * move_speed);
         const y = (data.y / 100 * move_speed);
         
-        const pos_x = Math.floor(pos.x + x);
-        const pos_y = Math.floor(pos.y + y);
+        const pos_x = (pos.x + x).toFixed(1);
+        const pos_y = (pos.y + y).toFixed(1);
         
         global.users[username].pos.x = pos_x;
         global.users[username].pos.y = pos_y;
