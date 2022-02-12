1f566579872289334123a34afbe4fb15b44c9b12
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-11 21:38:52 -0500

    update loader.js

--- logic/loader.js
+++ logic/loader.js
@@ -8,7 +8,7 @@ const users = () => {
     console.time("Users loaded in");
     const accounts = fs.readdirSync(config.DB + "/accounts/");
     for (let acc of accounts) {
-        global.users[acc.replace(".json", "")] = JSON.stringify(fs.readFileSync(config.DB + "/accounts/" + acc, "utf-8"));
+        global.users[acc.replace(".json", "")] = JSON.parse(fs.readFileSync(config.DB + "/accounts/" + acc, "utf-8"));
     }
     console.timeEnd("Users loaded in");
 };
@@ -17,7 +17,7 @@ const objects = () => {
     console.time("Objects loaded in");
     const _objects = fs.readdirSync(config.DB + "/objects/");
     for (let obj of _objects) {
-        global.objects[obj.replace(".json", "")] = JSON.stringify(fs.readFileSync(config.DB + "/objects/" + obj, "utf-8"));
+        global.objects[obj.replace(".json", "")] = JSON.parse(fs.readFileSync(config.DB + "/objects/" + obj, "utf-8"));
     }
     console.timeEnd("Objects loaded in");
 };
@@ -27,7 +27,7 @@ const world = () => {
     console.time("World loaded in");
     const worlds = fs.readdirSync(config.DB + "/maps/");
     for (let map of worlds) {
-        global.world[map.replace(".json", "")] = JSON.stringify(fs.readFileSync(config.DB + "/maps/" + map, "utf-8"));
+        global.world[map.replace(".json", "")] = JSON.parse(fs.readFileSync(config.DB + "/maps/" + map, "utf-8"));
     }
     console.timeEnd("World loaded in");
 };
@@ -37,7 +37,7 @@ const items = () => {
     console.time("Items loaded in");
     const _items = fs.readdirSync(config.DB + "/items/");
     for (let item of _items) {
-        global.items[item.replace(".json", "")] = JSON.stringify(fs.readFileSync(config.DB + "/items/" + item, "utf-8"));
+        global.items[item.replace(".json", "")] = JSON.parse(fs.readFileSync(config.DB + "/items/" + item, "utf-8"));
     }
     console.timeEnd("Items loaded in");
 };
