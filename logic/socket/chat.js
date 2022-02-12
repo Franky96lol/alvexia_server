1bbaa343c612620e52d0bc6cedfd71c869f74176
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-11 20:20:57 -0500

    update chat.js

--- logic/socket/chat.js
+++ logic/socket/chat.js
@@ -5,7 +5,7 @@ const config = require("../../config.js");
 const chat = (io , socket , username) => {
     socket.on("message" , (data) => {
         io.to(data.room).emit("message" , {
-            to : data.room,
+            room : data.room,
             from : global.users[username].nickname,
             type : data.type,
             message : data.message
