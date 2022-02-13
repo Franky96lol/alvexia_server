2b75b829aecf389462b7db1c1f121cd609933b23
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-13 15:54:15 -0500

    update movement.js

--- logic/socket/movement.js
+++ logic/socket/movement.js
@@ -6,15 +6,15 @@ const movement = (io , socket , username) => {
     socket.on("move_pj" , (data) => {
         const pos = global.users[username].pos;
         const move_speed = 1;
-        if(pos.last_step > (new Date().getTime() - config.RATE.ms)){
+        /*if(pos.last_step > (new Date().getTime() - config.RATE.ms)){
             return;
-        }
+        }*/
         global.users[username].pos.last_step = new Date().getTime();
-        const x = (data.x / 100 * move_speed);
-        const y = (data.y / 100 * move_speed);
+        /*const x = (data.x / 100 * move_speed);
+        const y = (data.y / 100 * move_speed);*/
         
-        let pos_x = parseInt(pos.x + x);
-        let pos_y = parseInt(pos.y + y);
+        let pos_x = data.x;//parseInt(pos.x + x);
+        let pos_y = data.y;//parseInt(pos.y + y);
        /* pos_x = pos_x.toFixed(1);
         pos_y = pos_y.toFixed(1);*/
         //console.log(username + " x:" + pos_x + ",y:" + pos_y);
