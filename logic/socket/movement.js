44b04d39ae2d92ebebce0e98f2c91d2a84b9cb1a
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-14 15:10:48 -0500

    update movement.js

--- logic/socket/movement.js
+++ logic/socket/movement.js
@@ -10,27 +10,16 @@ const movement = (io , socket , username) => {
             return;
         }*/
         global.users[username].pos.last_step = new Date().getTime();
-        /*const x = (data.x / 100 * move_speed);
-        const y = (data.y / 100 * move_speed);*/
-        
+
         let pos_x = data.x;//parseInt(pos.x + x);
         let pos_y = data.y;//parseInt(pos.y + y);
-       /* pos_x = pos_x.toFixed(1);
-        pos_y = pos_y.toFixed(1);*/
-        //console.log(username + " x:" + pos_x + ",y:" + pos_y);
+
         global.users[username].pos.x = pos_x;
         global.users[username].pos.y = pos_y;
         global.world[pos.map].pjs[username].pos.x = pos_x;
         global.world[pos.map].pjs[username].pos.y = pos_y;
         
-        io.to("map_" + pos.map).emit("move_pj", {
-            username : username,
-            pos : {
-                x : pos_x,
-                y : pos_y,
-                angle : 180
-            }
-        })
+        io.to("map_" + pos.map).emit("move_pj", username + "&" + pos_x + "&" + pos_y + "&" + 0);
         
     });
 };
