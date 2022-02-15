fe70aaebad3fdb507666d57f658ee92200684b3e
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-15 01:47:13 -0500

    update world_map.js

--- logic/install/world_map.js
+++ logic/install/world_map.js
@@ -4,37 +4,47 @@ const config = require("../../config.js");
 const uid = require(config.LOGIC + "/uid.js")
 const fs = require("fs");
 
-const generate = (map , mmap) => {
+const generate = (map, mmap) => {
     console.time("Map Generated in");
-    for(let y = 0 ; y < map ; y++){
-        for(let x = 0 ; x < map ; x++){
+    for (let y = 0; y < map; y++) {
+        for (let x = 0; x < map; x++) {
             let map = {
-                name : "",
-                pos : {
-                    x : x,
-                    y : y
+                name: "",
+                pos: {
+                    x: x,
+                    y: y
                 },
-                size : {
-                    x : mmap,
-                    y : mmap
+                size: {
+                    x: mmap,
+                    y: mmap
                 },
-                biome : "nature",
-                type : "neutral",
-                terrain : {},
-                objects : {},
-                npcs : {},
-                pjs : {}
+                biome: "nature",
+                type: "neutral",
+                terrain: {},
+                objects: {},
+                npcs: {},
+                pjs: {}
             };
-            for(let _y = 0; _y < mmap ; _y ++){
-                for(let _x = 0; _x < mmap ; _x ++){
-                    if(_y == 0 || _y == (mmap-1) &&
-                    _x == 0 || _x == (mmap - 1)) objects[]
-                }
-            }
-            fs.writeFileSync(config.DB + "/maps/" + x + "_" + y + ".json" , JSON.stringify(map) ,"utf-8");
-        }
-    }
-    console.timeEnd("Map Generated in");
-}
+            for (let _y = 0; _y < mmap; _y ++) {
+                for (let _x = 0; _x < mmap; _x ++) {
+                    if (_y == 0 || _y == (mmap-1) &&
+                        _x == 0 || _x == (mmap - 1)) objects[uid.alphanum(4)] = {
+                        name: "tree_1",
+                        faction: "silver_legion",
+                        amount: 1000,
+                        c_amount: 1000,
+                        type: "ind",
+                        pos: {
+                            x: _x,
+                            y: _y
+                        }
 
-module.exports = generate;
+                    }
+                }
+                fs.writeFileSync(config.DB + "/maps/" + x + "_" + y + ".json", JSON.stringify(map), "utf-8");
+            }
+        }
+        console.timeEnd("Map Generated in");
+    }
+
+    module.exports = generate;
