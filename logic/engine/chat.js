8f4d718a631f73afa315137873f4801860050b50
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-27 16:20:02 -0500

    update chat.js

--- logic/engine/chat.js
+++ logic/engine/chat.js
@@ -2,3 +2,25 @@
 
 const fs = require("fs");
 const config = require("../../config.js");
+
+class ChatEngine{
+    constructor(){
+        this.chat = {
+            global = [],
+            comerce = [],
+            system = [],
+            staff = [],
+            party = {},
+            private = {}
+        }
+    }
+    
+    load(){
+        const chats = fs.readdirSync(config.DB + "/chats/");
+        for(let chat of chats){
+            
+        }
+    }
+};
+
+module.exports = ChatEngine;
