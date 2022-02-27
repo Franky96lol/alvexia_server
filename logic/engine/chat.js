0b2601f8af11dd38a107e0bda0f57ddc2f9432c6
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-02-27 17:15:08 -0500

    update chat.js

--- logic/engine/chat.js
+++ logic/engine/chat.js
@@ -2,32 +2,46 @@
 
 const fs = require("fs");
 const config = require("../../config.js");
+const {
+    TaskTimer
+} = require("tasktimer");
 
-class ChatEngine{
-    constructor(){
+class ChatEngine {
+    constructor() {
         this.chats = {
-            global : [],
-            comerce : [],
-            system : [],
-            staff : [],
-            partys : {},
-            guilds : {},
-            privates : {}
+            global: [],
+            comerce: [],
+            system: [],
+            staff: [],
+            partys: {},
+            guilds: {},
+            privates: {}
         }
     }
     /* Load Chats from db*/
-    load(){
+    load() {
         console.time("Chats loaded in ");
-        for(let _chat in this.chats){
+        for (let _chat in this.chats) {
             this.chats[_chat] = JSON.parse(fs.readFileSync(config.DB + "/chats/" + _chat + ".json"));
         }
         console.timeEnd("Chats loaded in ");
     }
     /* Save Chats to db */
-    save(){
-        
+    save() {
+        const timer = new TaskTimer(1000);
+        timer.add([{
+            id: 'save_chat',
+            tickInterval: config.RATE.chat_save,
+            totalRuns: 0,
+            callback(task) {
+                for (let _chat in this.chats) {
+                    fs.writeFile(config.DB + "/chats/" + _chat + ".json", JSON.stringify(this.chats[_chat]), ()=> {});
+                }
+            }
+        }]);
+        timer.start();
     }
-    
+
 };
 
 module.exports = ChatEngine;
