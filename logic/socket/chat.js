83fa4d667345c18108b29a5611638812f30c77b9
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-02 10:13:56 -0500

    update chat.js

--- logic/socket/chat.js
+++ logic/socket/chat.js
@@ -7,19 +7,30 @@ ChatEngine.load();
 const chat = (io , socket , username) => {
     ChatEngine.loadChat(io , username , socket);
     /* On Message */
-    /* @params String(type , room , typem , message) */
+    /* @params String(type) 
+     * @params String(room)
+     * @params String(typem)
+     * @params String(message)
+     */
     socket.on("message" , async (data) => {
         data = data.split("&");
         await ChatEngine.send(io , data[0] , data[1], username , global.users[username].nickname , data[2] , data[3]);
     });
-    
+    /* On Join Channel */
+    /* @params String(room) */
     socket.on("join" , async (room) => {
         await ChatEngine.join(username , room);
     });
-    
+    /* On Leave Channel */
+    /* @params String(room) */
     socket.on("leave" , async (room) => {
         await ChatEngine.leave(username , room);
     });
+    /* On Join Private Channel */
+    /* @params String(username) */
+    socket.on("joinPrivate" , async(username2) =>{
+        await ChatEngine.joinPrivate(io , username , username2);
+    })
 };
 
 module.exports = chat;
