516136e177739703aebd21a65d6c112edd58ce33
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-02 10:39:18 -0500

    update socketing.js

--- logic/socketing.js
+++ logic/socketing.js
@@ -5,6 +5,7 @@ const socketing = (io , socket , username) => {
     require(config.LOGIC + "/socket/connection.js")(io, socket , username);
     require(config.LOGIC + "/socket/disconnect.js")(io , socket , username);
     require(config.LOGIC + "/socket/movement.js")(io , socket , username);
+    require(config.LOGIC + "/socket/chat.js")(io , socket , username);
 };
 
 module.exports = socketing;
