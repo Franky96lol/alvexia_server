6bf3464fb546957b370cd655cbcd98b5b16a16ac
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-03 16:28:19 -0500

    update statscalc.js

--- logic/engine/statscalc.js
+++ logic/engine/statscalc.js
@@ -2,12 +2,10 @@
 const config = require("../../config.js");
 
 const stats_per_attr = {
-    hp : {
-        hp : 10,
+    sta : {
+        hp : 5,
         hp_reg : 0.05
-    },
-    mp : {
-        mp : 10,
+        mp : 5,
         mp_reg : 0.05
     },
     str : {
@@ -35,51 +33,64 @@ const stats_per_attr = {
     }
 }
 
+const pos_attr = ["sta" , "str" , "agi" , "int" , "luck"];
+const pos_stats = ["hp" , "hp_reg" , "mp" , "mp_reg" , "phy_dmg" , "phy_def" , "mgc_dmg" , "mgc_def" , "crit" , "dodge" , "extra_gold" , "extra_xp" , "extra_drop"];
+
 function statscalc (username) {
     const base_attr = {
-        hp: 0,
-        mp: 0,
+        sta: 0,
         str: 0,
         agi: 0,
         int: 0,
         luck: 0
     };
     const base_stats = {
-        hp : 0,
-        hp_reg : 0,
-        mp : 0,
-        mp_reg : 0,
-        phy_dmg : 0,
-        phy_def : 0,
-        mgc_dmg : 0,
-        mgc_def : 0,
-        crit : 0,
-        dodge : 0,
+        hp : 50,
+        hp_reg : 1,
+        mp : 50,
+        mp_reg : 1,
+        phy_dmg : 5,
+        phy_def : 1,
+        mgc_dmg : 5,
+        mgc_def : 1,
+        crit : 1,
+        dodge : 1,
         extra_gold : 0,
         extra_xp : 0,
         extra_drop : 0
     };
     const acc = global.users[username];
-    delete acc.attributes.points;
     const attr = acc.attributes;
-    const status = acc.status;
+    const _status = acc.status;
     const equipment = acc.equiped;
+    
+    for (let a in base_attr){
+        base_attr += attr[a];
+    }
 
     for (let equip in equipment) {
-        for (let a in attr) {
-            attr[a] += global.items[equipment[equip]][a];
+        for (let a in equip) {
+            if(pos_attr.includes(a)){
+                base_attr += equip[a];
+            }else if (pos_stats.includes(a)){
+                base_stats += equip[a];
+            }
         }
     }
-
-    const exp = {
-        xp: Math.floor(Math.pow((acc.level + 4), 3))
+    
+    for (let a in base_attr){
+        for(let b in stats_per_attr[a]){
+            base_stats[b] += (stats_per_attr[a][b] * base_attr[a]);
+        }
+    }
+    
+    const xp = Math.floor(Math.pow((acc.level + 4), 3));
+    global.stats[username] = {
+        attr : base_attr,
+        stats : base_stats,
+        xp : xp
     };
 
-    return {
-        attr,
-        status,
-        exp
-    }
 };
 
 module.exports = statscalc;
