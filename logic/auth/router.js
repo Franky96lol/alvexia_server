67838ecdc780cf7cfae46e5459605ef9376471a8
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-03 17:19:21 -0500

    update router.js

--- logic/auth/router.js
+++ logic/auth/router.js
@@ -3,10 +3,16 @@ const register = require("./register.js");
 const login = require("./login.js");
 const verifyMail = require("./verifyMail.js");
 
-router.post("/register", (req , res) => register(req , res)); 
+router.post("/register", function (req , res) {
+    register(req , res)
+}); 
 
-router.post("/login" , (req , res) => login(req , res));
+router.post("/login" , function (req , res) {
+    login(req , res)
+});
 
-router.get("/verify/:user/:id" , (req , res) => verifyMail(req ,res));
+router.get("/verify/:user/:id" , function (req , res) { 
+    verifyMail(req ,res)
+});
 
 module.exports = router;
