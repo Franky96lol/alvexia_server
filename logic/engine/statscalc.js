7f1331cce205e6cab952da6477a11a347513b1b0
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-03 10:16:05 -0500

    update statscalc.js

--- logic/engine/statscalc.js
+++ logic/engine/statscalc.js
@@ -1,32 +1,34 @@
 /* Stadistics Calculator */
 const config = require("../../config.js");
 
-const base_stats = {
-    hp : 0,
-    mp : 0,
-    str : 0,
-    agi : 0,
-    int : 0,
-    luck : 0
-}
-
-const statscalc = (username) => {
+function statscalc (username) {
+    const base_stats = {
+        hp: 0,
+        mp: 0,
+        str: 0,
+        agi: 0,
+        int: 0,
+        luck: 0
+    };
+    const base_statistics = {
+        
+    };
     const acc = global.users[username];
     delete acc.attributes.points;
     const attr = acc.attributes;
     const status = acc.status;
     const equipment = acc.equiped;
-    
-    for(let equip in equipment){
-        for(let a in attr){
+
+    for (let equip in equipment) {
+        for (let a in attr) {
             attr[a] += global.items[equipment[equip]][a];
         }
     }
-    
+
     const exp = {
-        xp : Math.floor(Math.pow((acc.level + 4) , 3))
+        xp: Math.floor(Math.pow((acc.level + 4), 3))
     };
-    
+
     return {
         attr,
         status,
