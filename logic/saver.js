205c294c3761066677b05113d245c8c855225f22
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-04 17:51:19 -0500

    update saver.js

--- logic/saver.js
+++ logic/saver.js
@@ -21,18 +21,18 @@ timer.add([{
     }
 }]);
 
-function users_save () {
+async function users_save () {
     console.time("Accounts database saved in");
     for(let user in global.users){
-        fs.writeFile(config.DB + "/accounts/" + user + ".json" , JSON.stringify(global.users[user]) , ()=>{});
+        await fs.writeFile(config.DB + "/accounts/" + user + ".json" , JSON.stringify(global.users[user]) , ()=>{});
     }
     console.timeEnd("Accounts database saved in");
 };
 
-function world_save (){
+async function world_save (){
     console.time("World saved in");
     for(let map in global.world){
-        fs.writeFile(config.DB + "/maps/" + map + ".json" , JSON.stringify(global.world[map]), ()=>{});
+        await fs.writeFile(config.DB + "/maps/" + map + ".json" , JSON.stringify(global.world[map]), ()=>{});
     }
     console.timeEnd("World saved in");
 };
