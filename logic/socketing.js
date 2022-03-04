89ea4094a10a1583f4fb049e147fa565db7dd47c
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-04 18:53:32 -0500

    update socketing.js

--- logic/socketing.js
+++ logic/socketing.js
@@ -3,11 +3,11 @@ const config = require("../config.js");
 const chat = require(config.LOGIC + "/socket/chat.js");
 
 async function socketing (io , socket , username){
-    require(config.LOGIC + "/socket/connection.js")(io, socket , username);
-    require(config.LOGIC + "/socket/disconnect.js")(io , socket , username);
-    require(config.LOGIC + "/socket/movement.js")(io , socket , username);
-    chat(io , socket , username);
-    require(config.LOGIC + "/socket/inventory.js")(io, socket , username);
+    await require(config.LOGIC + "/socket/connection.js")(io, socket , username);
+    await require(config.LOGIC + "/socket/disconnect.js")(io , socket , username);
+    await require(config.LOGIC + "/socket/movement.js")(io , socket , username);
+    await chat(io , socket , username);
+    await require(config.LOGIC + "/socket/inventory.js")(io, socket , username);
 };
 
 module.exports = socketing;
