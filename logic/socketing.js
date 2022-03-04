7d9bc218c4357a81a631d62b049a158006216d41
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-04 14:01:45 -0500

    update socketing.js

--- logic/socketing.js
+++ logic/socketing.js
@@ -2,7 +2,7 @@
 const config = require("../config.js");
 const chat = require(config.LOGIC + "/socket/chat.js");
 
-function socketing (io , socket , username){
+async function socketing (io , socket , username){
     require(config.LOGIC + "/socket/connection.js")(io, socket , username);
     require(config.LOGIC + "/socket/disconnect.js")(io , socket , username);
     require(config.LOGIC + "/socket/movement.js")(io , socket , username);
