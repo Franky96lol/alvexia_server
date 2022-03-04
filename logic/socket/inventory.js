6b28d682a1cde2f74f974dd8d68d1cf01630c4bb
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-04 13:57:44 -0500

    update inventory.js

--- logic/socket/inventory.js
+++ logic/socket/inventory.js
@@ -1,1 +1,26 @@
 /* On Inventory*/
+
+const config = require("../../config.js");
+const inv = require(config.LOGIC + "/engine/inventory.js");
+
+function inventory(io , socket , username){
+    socket.on("inventory" , async function (data){
+        switch(data){
+            case "armory":
+                socket.emit("armory" , await inv.armory(username));
+                return;
+                break;
+            case "consumables":
+                socket.emit("consumables" , await inv.consumables(username));
+                return;
+                break;
+            case "materials":
+                socket.emit("materials" , await inv.materials(username));
+                return;
+                break;
+            default :
+                return;
+                break;
+        };
+    });
+}
