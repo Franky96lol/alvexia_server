1a5ca7003aa68bbc8aef65110bceb831ac1e2173
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-03 22:23:02 -0500

    update inventory.js

--- logic/engine/inventory.js
+++ logic/engine/inventory.js
@@ -1,1 +1,28 @@
-/* Inventory */
+/* Inventory Engine */
+
+const config = require("../../config.js");
+
+function armory(username){
+    let armory = {};
+    for(let item of global.users[username].armory){
+        armory[item] = global.items[item];
+    }
+    return return {bags : global.users[username].bags , items : armory};
+}
+
+function consumables(username){
+    let consumables = {};
+    for(let item of global.users[username].consumables){
+        consumables[item] = global.items[item];
+    }
+    return {bags : global.users[username].bags , items : consumables};
+}
+
+function materials(username){
+    let materials = {};
+    for(let item of global.users[username].materials){
+        materials[item] = global.items[item];
+    }
+    return {bags : global.users[username].bags , items : materials};
+}
+
