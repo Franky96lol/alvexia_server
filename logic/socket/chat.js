74fa89665048b028c760f21b0190c05e73bfc6df
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-04 18:57:18 -0500

    update chat.js

--- logic/socket/chat.js
+++ logic/socket/chat.js
@@ -4,36 +4,36 @@ const config = require("../../config.js");
 const ChatEngine = new (require(config.LOGIC + "/engine/chat.js"))();
 ChatEngine.load();
 
-function chat (io , socket , username) {
-    ChatEngine.loadChat(io , username , socket);
+async function chat (io , socket , username) {
+    await ChatEngine.loadChat(io , username , socket);
     /* On Message */
     /* @params String(type) 
      * @params String(room)
      * @params String(typem)
      * @params String(message)
      */
-    socket.on("message" , async function (data){
+    await socket.on("message" , async function (data){
         data = data.split("&");
         await ChatEngine.send(io , data[0] , data[1], username , global.users[username].nickname , data[2] , data[3]);
     });
     /* On Join Channel */
     /* @params String(room) */
-    socket.on("join" , async function (room) {
+    await socket.on("join" , async function (room) {
         await ChatEngine.join(username , room);
     });
     /* On Leave Channel */
     /* @params String(room) */
-    socket.on("leave" , async function (room) {
+    await socket.on("leave" , async function (room) {
         await ChatEngine.leave(username , room);
     });
     /* On Join Private Channel */
     /* @params String(username2) */
-    socket.on("joinPrivate" , async function(username2) {
+    await socket.on("joinPrivate" , async function(username2) {
         await ChatEngine.joinPrivate(io , username , username2);
     });
     /* On Leave Private Channel */
     /* @params String(room) */
-    socket.on("leavePrivate" , async function (room) {
+    await socket.on("leavePrivate" , async function (room) {
         await ChatEngine.leavePrivate(io , username , room);
     });
 };
