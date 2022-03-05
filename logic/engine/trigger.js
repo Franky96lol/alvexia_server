75bc6f5017878bfd51b918fab3755035bfa939a9
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-05 14:55:54 -0500

    update trigger.js

--- logic/engine/trigger.js
+++ logic/engine/trigger.js
@@ -3,9 +3,47 @@
 const config = require("../..config.js");
 
 const Trigger = {
-    
-    ca : function (io , socket , username , Trigger) {
-        
+    /* On Change Area
+     * @params Socket(io)
+     * @params Socket(socket)
+     * @params String(username)
+     * @params String(trigger) */
+    ca: async function (io, socket, username, trigger) {
+        trigger = trigger.split("&");
+        const pos = {
+            map : trigger[0],
+            x : trigger[1].split("_")[0],
+            y : trigger[1].split("_")[1],
+            angle : 0,
+            last_step : new Date().getTime()
+        };
+        const acc = await global.users[username];
+        socket.broadcast.to("map_" + acc.pos.map).emit("del_pj", username);
+        delete global.world[acc.pos.map].pjs[username];
+        socket.leave("map_" + acc.pos.map);
+        const _stats = await global.stats[username];
+        const pjstats = {
+            nickname: acc.nickname,
+            status: {
+                level: acc.level,
+                xp: _stats.xp,
+                c_xp: acc.xp,
+                hp: _stats.stats.hp,
+                mp: _stats.stats.mp,
+                c_hp: acc.status.hp,
+                c_mp: acc.status.mp,
+                speed: _stats.stats.speed
+            },
+            size: acc.size,
+            skin: acc.skin,
+            pos: pos
+        };
+        await socket.join("map_" + pos.map);
+        socket.broadcast.to("map_" + pos.map).emit("new_pj", {
+            username: username, pjstats: pjstats
+        });
+        global.world[pos.map].pjs[username] = pjstats;
+        socket.emit("load_map", global.world[pos.map]);
     }
 }
 
