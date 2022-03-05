ec58ae1e9b5b9ac9b0b0578d38e468293aad0eeb
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-05 03:16:12 -0500

    update admin.js

--- logic/engine/admin.js
+++ logic/engine/admin.js
@@ -2,33 +2,39 @@
 
 const config = require("../../config.js");
 
-async function gm(io , socket , username , ChatEngine){
-    if(config.STAFF.moderator.includes(username)){
-        if(global.users[username].acclevel < 4){
-            global.users[username].acclevel = 4;
-            await socket.emit("gm" , "on");
-            await ChatEngine.send(io , "staff" , "staff", "Sistema" , "Sistema" , "text" , "El usuario " + username + " activo el modo gm (" + global.users[username].acclevel + ")");
-            return;
-        }else{
-            global.users[username].acclevel = 1;
-            await socket.emit("gm" , "off");
-            await ChatEngine.send(io , "staff" , "staff", "Sistema" , "Sistema" , "text" , "El usuario " + username + " desactivo el modo gm (" + global.users[username].acclevel + ")");
-            return;
-        }
+class Admin {
+    constructor(ChatEngine) {
+        this.ChatEngine = ChatEngine;
     }
-    else if(config.STAFF.moderator.includes(username)){
-        if(global.users[username].acclevel < 3){
-            global.users[username].acclevel = 3;
-            await socket.emit("gm" , "on");
-            await ChatEngine.send(io , "staff" , "staff", "Sistema" , "Sistema" , "text" , "El usuario " + username + " activo el modo gm (" + global.users[username].acclevel + ")");
-            return;
-        }else{
-            global.users[username].acclevel = 1;
-            await socket.emit("gm" , "off");
-            await ChatEngine.send(io , "staff" , "staff", "Sistema" , "Sistema" , "text" , "El usuario " + username + " desactivo el modo gm (" + global.users[username].acclevel + ")");
-            return;
+
+    async gm(io, socket, username) {
+        if (config.STAFF.moderator.includes(username)) {
+            if (global.users[username].acclevel < 4) {
+                global.users[username].acclevel = 4;
+                await socket.emit("gm", "on");
+                await this.ChatEngine.send(io, "staff", "staff", "Sistema", "Sistema", "text", "El usuario " + username + " activo el modo gm (" + global.users[username].acclevel + ")");
+                return;
+            } else {
+                global.users[username].acclevel = 1;
+                await socket.emit("gm", "off");
+                await this.ChatEngine.send(io, "staff", "staff", "Sistema", "Sistema", "text", "El usuario " + username + " desactivo el modo gm (" + global.users[username].acclevel + ")");
+                return;
+            }
+        } else if (config.STAFF.moderator.includes(username)) {
+            if (global.users[username].acclevel < 3) {
+                global.users[username].acclevel = 3;
+                await socket.emit("gm", "on");
+                await this.ChatEngine.send(io, "staff", "staff", "Sistema", "Sistema", "text", "El usuario " + username + " activo el modo gm (" + global.users[username].acclevel + ")");
+                return;
+            } else {
+                global.users[username].acclevel = 1;
+                await socket.emit("gm", "off");
+                await this.ChatEngine.send(io, "staff", "staff", "Sistema", "Sistema", "text", "El usuario " + username + " desactivo el modo gm (" + global.users[username].acclevel + ")");
+                return;
+            }
         }
     }
 }
-
-module.exports = {gm};
+module.exports = {
+    gm
+};
