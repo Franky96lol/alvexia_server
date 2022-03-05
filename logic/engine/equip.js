9863635b8ed07241e2a7408998d9f6054628a7ce
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-04 19:36:10 -0500

    update equip.js

--- logic/engine/equip.js
+++ logic/engine/equip.js
@@ -1,1 +1,11 @@
 /*Equip Engine*/
+
+async function equip(username , item , slot){
+    if(slot == undefined || slot == null){
+        const acc = global.users[username];
+        const sta = global.stats[username].concat({level : acc.level});
+        const item = global.items[acc.armory[item].id]
+    }else{
+        
+    }
+}
