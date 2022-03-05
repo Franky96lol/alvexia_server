5229b097289ca1145967efebd670cf6c1a78e6d4
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-05 03:06:07 -0500

    update chat.js

--- logic/socket/chat.js
+++ logic/socket/chat.js
@@ -3,7 +3,7 @@
 const config = require("../../config.js");
 const ChatEngine = new (require(config.LOGIC + "/engine/chat.js"))();
 ChatEngine.load();
-const admin = require(config.LOGIC + "/engine/admin.js");
+const admin = new (require(config.LOGIC + "/engine/admin.js"))(ChatEngine);
 
 async function chat (io , socket , username) {
     await ChatEngine.loadChat(io , username , socket);
@@ -17,12 +17,14 @@ async function chat (io , socket , username) {
         data = data.split("&");
         if(global.users[username].acclevel >= 3){
             if(data[3] == "/gm"){
-                await admin.gm(io , socket , username , ChatEngine);
+                await admin.gm(io , socket , username);
                 return;
             }else if(data[3].includes("/tele ") && data[3].split(" ").length == 2){
-                await admin.tele(io , socket , username , ChatEngine)
+                await admin.tele(io , socket , username , data[3].split(" ")[1]);
+                return;
             }else if(data[3].includes("/ban ") && data[3].split(" ").length == 2){
-                
+                await admin.ban(io , socket , username , data[3].split(" ")[1]);
+                return;
             }
         }
         await ChatEngine.send(io , data[0] , data[1], username , global.users[username].nickname , data[2] , data[3]);
