3804b8f4b0eb855fb598718e40ad9b273055aa69
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-03-05 14:41:24 -0500

    update disconnect.js

--- logic/socket/disconnect.js
+++ logic/socket/disconnect.js
@@ -2,11 +2,11 @@
 
 const config = require("../../config.js");
 
-const disconnect = (io, socket, username) => {
-    socket.on("disconnect", (data) => {
-        socket.broadcast.to("map_" + global.users[username].pos.map).emit("del_pj", username);
+async function disconnect (io, socket, username){
+    await socket.on("disconnect", async function (data) {
+        await socket.broadcast.to("map_" + global.users[username].pos.map).emit("del_pj", username);
         delete global.world[global.users[username].pos.map].pjs[username];
-        socket.leave("map_" + global.users[username].pos.map);
+        await socket.leave("map_" + global.users[username].pos.map);
         global.users[username].isOnline = false;
         return;
     });
